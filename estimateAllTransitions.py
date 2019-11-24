ed6977fb35ac7a036b44e61251903410dcd51c3f
rodri651 <rodri651@seas.upenn.edu>
2019-11-23 23:32:15 -0500

    with suggested changes

--- estimateAllTransitions.py
+++ estimateAllTransitions.py
@@ -20,9 +20,8 @@ class estimateTranslation():
 		Do remember x represents column & y= row. 
 		I am only making 5 iteration to get more accurate results. this can be changed 
 		by changing value of variable Iterations''' 
-		print("here")
+		
 		It = img2 - img1
-		#pdb.set_trace()
 		length, width = np.shape(img2)
 		sobel_x = np.array([[-1, 0, +1],[-2, 0, +2],[-1, 0, +1]])
 		sobel_y = np.array([[-1, -2, -1],[0, 0, 0],[+1, +2, +1]])
@@ -33,56 +32,99 @@ class estimateTranslation():
 		It_interp = interpolate.interp2d(x_range,y_range,It,kind='cubic')
 		Ix_interp = interpolate.interp2d(x_range,y_range,Ix,kind='cubic')
 		Iy_interp = interpolate.interp2d(x_range,y_range,Iy,kind='cubic')
-		#pdb.set_trace()
-		Iterations = 5
+		
+		indexes = np.ones((len(self.startXs)))
+		n_values=0
 		for i in range(len(self.startXs)):
 			x = self.startXs[i]
 			y = self.startYs[i]
-			for j in range(Iterations):
-				
-				#It_small = It[y-4:y+6,x-4:x+6]
-				x_interp = np.arange(y-4,y+6)
-				y_interp = np.arange(x-4,x+6)
-				It_small = It_interp(y_interp,x_interp)
-				it = It_small.flatten()
-				Ix_small = Ix_interp(y_interp,x_interp)
-				#Ix_small = Ix[y-4:y+6,x-4:x+6]
-				ix = Ix_small.flatten()
-				#Iy_small = Iy[y-4:y+6,x-4:x+6]
-				Iy_small = Iy_interp(y_interp,x_interp)
-				iy = Iy_small.flatten()
-				a = np.square(ix)
-				a = np.sum(a)	
-				b = np.matmul(np.reshape(ix,(1,len(ix))),np.reshape(iy,(len(iy),1)))
-				c = np.square(iy)
-				c = np.sum(c)
-				e = np.matmul(np.reshape(ix,(1,len(ix))),np.reshape(it,(len(it),1)))
-				f = np.matmul(np.reshape(iy,(1,len(iy))),np.reshape(it,(len(it),1)))
-				A = np.array([[a,b[0,0]],[b[0,0],c]])
-				B = np.array([e,f])
-				B = np.reshape(B,(2,1))
-				u_v = np.matmul(np.linalg.inv(A),B)
-				x = x + u_v[0]
-				y = y + u_v[1]
-				#pdb.set_trace()
-			if (x <= width):
+			error=100
+			#for j in range(Iterations):
+			iterations=0
+			while(error>0.0) and iterations<6:	
+			
+				try:
+					#It_small = It[y-4:y+6,x-4:x+6]
+					x_interp = np.arange(y-9,y+10)
+					y_interp = np.arange(x-9,x+10)
+					
+					It_small = It_interp(y_interp,x_interp)
+					it = It_small.flatten()
+					Ix_small = Ix_interp(y_interp,x_interp)
+					#Ix_small = Ix[y-4:y+6,x-4:x+6]
+					ix = Ix_small.flatten()
+					#Iy_small = Iy[y-4:y+6,x-4:x+6]
+					Iy_small = Iy_interp(y_interp,x_interp)
+					iy = Iy_small.flatten()
+					a = np.square(ix)
+					a = np.sum(a)	
+					#b = np.matmul(np.reshape(ix,(1,len(ix))),np.reshape(iy,(len(iy),1)))
+					b=np.sum(ix*iy)
+					c = np.square(iy)
+					c = np.sum(c)
+					#e = np.matmul(np.reshape(ix,(1,len(ix))),np.reshape(it,(len(it),1)))
+					e = np.sum(ix*it)
+					#f = np.matmul(np.reshape(iy,(1,len(iy))),np.reshape(it,(len(it),1)))
+					f = np.sum(iy*it)
+					A = np.array([[a,b],[b,c]])
+					#pdb.set_trace()
+					B = np.array([e,f])
+					B = np.reshape(B,(2,1))
+					u_v = np.matmul(np.linalg.inv(A),B)
+					x = x + u_v[0]
+					y = y + u_v[1]
+					#pdb.set_trace()
+					A_error = np.hstack((np.reshape(ix,(len(ix),1)),np.reshape(iy,(len(iy),1))))
+					b_error = np.reshape(it,(len(it),1))
+					error = np.linalg.norm(np.matmul(A_error,u_v)+b_error,ord=2)
+					#error = np.linalg.norm(u_v,ord=2)
+					#print(error)
+					iterations+=1
+					#if iterations%100==0:
+				#		print(iterations,x,y,u_v)
+				except:
+					print("failed")
+
+			if (x <= width) and (y<=length) and error<0.10:
 				self.newXs.append(x)
-			else:
-				print("x is out of range for this case")
-			if (y <= length):
 				self.newYs.append(y)
+				n_values+=1
+				#print("[IN Range]")
 			else:
-				print("y is out of range for this case")
-			pdb.set_trace()
+				#print("x is out of range for this case")
+				indexes[i]=0
+				
+		#pdb.set_trace()
+		self.startXs = self.startXs[indexes==1]
+		self.startYs = self.startYs[indexes==1]
+		self.newXs = np.array(self.newXs)
+		self.newYs = np.array(self.newYs)
 		return self.newXs, self.newYs
 
 	def applyGeometricTransformation(self, bbox):
 		'''This is to predict new bounding box coordinates. 
 		Input bbox = np.array([x,y],[x,y],[x+w,y+h],[x+w,y+h])'''
-		src = np.array([np.array(self.startXs),np.array(self.startYs)])
-		dst = np.array([np.array(self.newXs), np.array(self.newYs)])
+		
+		length = len(self.startXs)
+		src = np.hstack((self.startXs.reshape(length,1),self.startYs.reshape(length,1)))
+		dst = np.hstack((self.newXs.reshape(length,1),self.newYs.reshape(length,1)))
+		#pdb.set_trace()
 		tform = tf.estimate_transform('similarity', src, dst)
-		self.new_bbox = warp(bbox,tform)
+		center_x = bbox[0]+bbox[2]/2
+		center_y = bbox[1]+bbox[3]/2
+		old_coord = np.array([center_x,center_y,1]).reshape(3,1)
+		new_coord = np.matmul(tform.params,old_coord)
+		diff = np.linalg.norm(new_coord-old_coord)
+		print("[values]",diff)
+		if diff>30 :
+			new_coord = old_coord.copy()
+		#new_center = warp(np.array([center_x,center_y]),tform)
+		
+		self.new_bbox = np.zeros((4,1))
+		self.new_bbox[0]= new_coord[0]-bbox[2]/2
+		self.new_bbox[1]= new_coord[1]-bbox[3]/2
+		self.new_bbox[2]= bbox[2]
+		self.new_bbox[3]= bbox[3]
 
 		return self.new_bbox
 
--- main.py
+++ main.py
@@ -1,32 +1,64 @@
 import pdb
 
+import glob
+
 from harris_corners import Harris_corners
 from estimateAllTransitions import estimateTranslation
 import bbox_create
 import cv2
 import numpy as np
 from PIL import Image	
+from scipy import signal
 
 verbose=False
+image_files = glob.glob("Easy/*.png")
+image_files.sort()
 
 if __name__ == '__main__':
-	image_1 = cv2.imread('Easy/0.png')
-	image_2 = cv2.imread('Easy/1.png')
+	image_1 = cv2.imread('Easy/000.png')
+	image_2 = cv2.imread('Easy/001.png')
 	
 	bbox = bbox_create.bounding_box()	
 	bboxes = bbox.create_bbox(image_1)
-	x,y,w,h = bboxes[0]
-	img_req_1 = cv2.cvtColor(image_1[y:y+h,x:x+w], cv2.COLOR_RGB2GRAY)
-	img_req_2 = cv2.cvtColor(image_2[y:y+h,x:x+w], cv2.COLOR_RGB2GRAY)
-	
-	H = Harris_corners(img_req_1)
-	gaussian_filter = H.gaussian_filter(1)
-	suppressed_image = H.corner_detector(0.04)
-	points_y, points_x= H.adap_supp_class(num_features=100)
-	
-	if verbose==True:
-		Image.fromarray((suppressed_image*255).astype(np.uint8)).show()
+	new_bbox = bboxes[0]
+	#pdb.set_trace()
+	#for i in range(len(image_files)-1):
+	for i in range(50):
+		x = int(new_bbox[0])
+		y = int(new_bbox[1])
+		w = int(new_bbox[2])
+		h = int(new_bbox[3])
+		
+		image_1 = cv2.imread(image_files[i])
+		image_2 = cv2.imread(image_files[i+1])
+		img_req_1 = cv2.cvtColor(image_1[y:y+h,x:x+w], cv2.COLOR_RGB2GRAY)
+		img_req_2 = cv2.cvtColor(image_2[y:y+h,x:x+w], cv2.COLOR_RGB2GRAY)
+		img_req_1 = img_req_1/255
+		img_req_2 = img_req_2/255
+		H = Harris_corners(img_req_1)
+		gaussian_filter = H.gaussian_filter(0.5)
+		suppressed_image = H.corner_detector(0.04)
+		points_y, points_x= H.adap_supp_class(num_features=50)
 
-	Translation = estimateTranslation(points_x,points_y)
-	Translation.estimateAllTranslation(img_req_1,img_req_2)
-	pdb.set_trace()
+		img_req_1 = signal.convolve2d(img_req_1,gaussian_filter,boundary='symm',mode='same')
+		img_req_2 = signal.convolve2d(img_req_2,gaussian_filter,boundary='symm',mode='same')
+		#corners = cv2.goodFeaturesToTrack(img_req_1,25,0.01,10)
+		#corners = np.int0(corners.squeeze())
+		#points_x = corners[:,0]
+		#points_y = corners[:,1]
+		if verbose==True:
+			Image.fromarray((suppressed_image*255).astype(np.uint8)).show()
+
+		Translation = estimateTranslation(points_x,points_y)
+		Translation.estimateAllTranslation(img_req_1,img_req_2)
+		new_bbox = Translation.applyGeometricTransformation(new_bbox)
+		
+		new_bbox = new_bbox.astype(np.uint)
+		start_point = (new_bbox[0],new_bbox[1])
+		end_point = (new_bbox[0]+new_bbox[2],new_bbox[1]+new_bbox[3])
+		image_1 = cv2.rectangle(image_1,start_point,end_point,(255,0,255),5)
+		Image.fromarray(image_1).show()
+		#pdb.set_trace()
+		#print(i)
+		#print(new_bbox)
+	# pdb.set_trace()
